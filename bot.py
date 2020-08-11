c47a54cfe552e7b6212b944fbfce4212eb5cc036
Iron <IronCodez@gmail.com>
2020-08-11 11:41:02 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,22 +17,5 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-
-@client.event
-async def on_ready():
-    print(f"Succesfully signed in as {client.user.name} ({client.user.id}).")
-    print('------')
-    #Status
-    activity = discord.Activity(
-        name=f'Paid My Dues | nf!help',
-        type=discord.ActivityType.streaming,
-        url="https://twitch.tv/nfmemes")
-    await client.change_presence(activity=activity)
-    print("Succesfully changed the status.")
-    #Booted?
-    channel = client.get_channel(720110932910538793)
-    await channel.send(f'Booted at {time.ctime()}')
-
-
 keep_alive.keep_alive()
 client.run(token)
