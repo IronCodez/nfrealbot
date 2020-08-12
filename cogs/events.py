9cc871b8851acaa8cc416c19e172fb3524693343
Iron <IronCodez@gmail.com>
2020-08-12 19:17:04 -0400

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -1,7 +1,5 @@
-import discord
+import discord, time
 from discord.ext import commands
-
-import time
 
 
 class events(commands.Cog):
@@ -21,9 +19,32 @@ class events(commands.Cog):
             url="https://twitch.tv/nfmemes")
         await self.client.change_presence(activity=activity)
         print("Succesfully changed the status.")
-        #Booted?
         channel = self.client.get_channel(720110932910538793)
         await channel.send(f'Booted at {time.ctime()}')
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = self.client.get_channel(719710156576915556)
+        await channel.send(f"**{member}** how could you leave us? :cry:")
+
+    @commands.command()
+    async def fakeleave(self, ctx, member: discord.Member):
+        await ctx.send(f"**{member}** how could you leave us? :cry:")
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        embed = discord.Embed(
+            color=discord.Color.red(),
+            timestamp=message.created_at,
+            description=
+            f"**Message sent by {message.author.mention} deleted in {message.channel.mention}** \n{message.content}"
+        )
+        embed.set_author(
+            name=f"{message.author}", icon_url=message.author.avatar_url)
+        embed.set_footer(
+            text=f"Author: {message.author.id} | Message ID: {message.id}")
+        channel = self.client.get_channel(707619715903914084)
+        await channel.send(embed=embed)
 
 
 def setup(bot):
